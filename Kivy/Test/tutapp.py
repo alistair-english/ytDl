d3c5029c3b739d6b4a558021b600c5ed45e96392
Alistair English <alistair.english@bigpond.com>
2017-01-12 13:16:46 +1000

    textbased ui

--- Kivy/Test/tutapp.py
+++ Kivy/Test/tutapp.py
@@ -11,7 +11,7 @@ from kivy.uix.boxlayout import BoxLayout
 class tutapp(App):
     def build(self):
         b = BoxLayout(orientation='vertical')
-        t = TextInput(font_size=150, size_hint_y=0.5, text='default')
+        t = TextInput(font_size=150, size_hint_y=None, height=200, text='default')
         f = FloatLayout()
         s = Scatter()
         l = Label(font_size=150, text='default')
